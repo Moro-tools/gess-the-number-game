9331ba61a760c32f406aecd2eb0cef4c01c88744
Moro dev <120253206+Moro-tools@users.noreply.github.com>
2023-08-11 01:16:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,22 @@ function checkGuess() {
   if (userGuess === randomNumber) {
     message.style.color = 'green';
     message.textContent = 'Congratulations! You guessed the correct number!';
+    let existingContainer = document.getElementById("myContainer");
+    var element = document.createElement("button");
+    element.className = "element";
+    element.textContent = "Play Again?";
+    element.addEventListener("click", function() {
+    location.reload();
+});
+
+element.className = "playAgain";
+existingContainer.appendChild(element);
   } else if (userGuess < randomNumber) {
     message.style.color = 'red';
     message.textContent = 'Too low. Try again.';
   } else {
     message.style.color = 'red';
     message.textContent = 'Too high. Try again.';
+
   }
 }
